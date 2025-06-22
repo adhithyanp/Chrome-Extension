1cb1f51461d3e416291a1d14d83bfddff4616257
Adhithyan0 <adhithyan43529@gmail.com>
2025-06-22 11:03:23 +0530

    added list tag

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 let myLeads = []
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click" , function save(){
-    console.log("Button clicked!")
-    myLeads.push("www.awesomelead.com")
+  
+    myLeads.push(inputEl.value)
     console.log(myLeads)
+   
 } )
 
+for(let i=0;i<myLeads.length;i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
+}
